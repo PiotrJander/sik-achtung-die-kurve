d0347e3bf18dc16f73400d89585729eb54204fd8
PiotrJander <pjander2@gmail.com>
2017-08-22 16:48:58 +0100

    Further refactors StreamUtils

--- src/nietacka/StreamUtils.h
+++ src/nietacka/StreamUtils.h
@@ -8,9 +8,10 @@
 #include <istream>
 
 using std::istream;
+using std::ostream;
 using std::streamsize;
 
-class InputStreamUtils {
+class StreamUtils {
 public:
     template<typename T>
     static T read_int(istream &s)
@@ -18,6 +19,12 @@ public:
         T ret;
         s.read((char *) &ret, sizeof(ret));
         return ret;
+    }
+
+    template<typename T>
+    static ostream &write_int(ostream &s, T n)
+    {
+        return s.write((char *) &n, sizeof(n));
     }
 };
 
